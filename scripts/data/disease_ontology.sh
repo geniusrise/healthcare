8e2cfc6dc9e2e610890945c804869efbbfbe8ed1
ixaxaar <root@ixaxaar.in>
2024-06-03 16:29:59 +0530

    add data fetching scripts

--- scripts/data/disease_ontology.sh
+++ scripts/data/disease_ontology.sh
@@ -3,3 +3,7 @@
 wget https://github.com/DiseaseOntology/HumanDiseaseOntology/raw/main/src/ontology/HumanDO.json
 wget https://github.com/DiseaseOntology/HumanDiseaseOntology/raw/main/src/ontology/HumanDO.obo
 wget https://github.com/DiseaseOntology/HumanDiseaseOntology/raw/main/src/ontology/HumanDO.owl
+
+wget https://github.com/DiseaseOntology/SymptomOntology/raw/main/src/ontology/symp.json
+wget https://github.com/DiseaseOntology/SymptomOntology/raw/main/src/ontology/symp.obo
+wget https://github.com/DiseaseOntology/SymptomOntology/raw/main/src/ontology/symp.owl
